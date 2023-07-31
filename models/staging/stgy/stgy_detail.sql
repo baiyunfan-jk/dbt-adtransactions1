a86692f9e665e5647709ea0ca359bd73fdb2eed3
baiyunfan-jk <140997291+baiyunfan-jk@users.noreply.github.com>
2023-07-31 16:18:27 +0800

    Update and rename stgy.sql to stgy_detail.sql

---
+++ models/staging/stgy/stgy_detail.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='view') }}
+with stgy as ( -- 策略中文名 排老黑2.0/PreA4.0低分高分差异化出价
+    select
+        config_id,
+        bucket,
+        input_date,
+        date_format(input_date,'yyyyMMdd')input_pday,
+        first(strategys_name) strategys_name
+    from
+(
+    select strategyName as strategys_name, agId as config_id, input_date, bucket
+    from fin_dm_data_ai.prd_glaucus_p_gl_account_decision_conf_sync_pda LATERAL VIEW EXPLODE(split(bucketIds, ",")) as bucket
+    where pday>= date_format(date_sub(date('${pDate}') ,8),'yyyyMMdd')
+)
+    group by 1,2,3,4
+)
+select * from stgy
