e816d1ea73806ea802501c6c5c04b0a8e1ccc5e0
baiyunfan-jk <140997291+baiyunfan-jk@users.noreply.github.com>
2023-08-01 17:21:15 +0800

    Update rta_cost_media_bckt_accgroup_request.sql

--- models/intermediate/rta_request_aggregated/rta_cost_media_bckt_accgroup_request.sql
+++ models/intermediate/rta_request_aggregated/rta_cost_media_bckt_accgroup_request.sql
@@ -9,9 +9,11 @@ stgy as (
   select * from {{ ref('glaucus_p_gl_account_decision_conf_sync_pda')}}
 ),
 spend as (
-select * from source
+select * from source a
 left join stgy cfg2
     on a.config_id = cfg2.config_id and a.bucket=cfg2.bucket --and date_format(a.dt,'yyyyMMdd')=cfg2.input_pday
     and from_unixtime(unix_timestamp(cfg2.input_pday,'yyyyMMdd'),'yyyy-MM-dd')=date_sub(a.dt,1)
 where dt >=date_sub(date('${pDate}') ,7)
 )
+
+select * from spend
