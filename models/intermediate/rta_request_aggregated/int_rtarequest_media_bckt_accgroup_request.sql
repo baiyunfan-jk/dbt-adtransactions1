1626c604eec8eb5b7fd4e8e2d03a5db08a0fe54d
baiyunfan-jk <140997291+baiyunfan-jk@users.noreply.github.com>
2023-08-01 11:48:38 +0800

    Update int_rtarequest_media_bckt_accgroup_request.sql

--- models/intermediate/rta_request_aggregated/int_rtarequest_media_bckt_accgroup_request.sql
+++ models/intermediate/rta_request_aggregated/int_rtarequest_media_bckt_accgroup_request.sql
@@ -1,1 +1,10 @@
+{{ config(materialized='ephemeral') }}
 
+with req as (
+
+   select * from {{ ref('rta_req_new_config_bckt') }}
+
+),
+stgy as (
+  select * from {{ ref('glaucus_p_gl_account_decision_conf_sync_pda')}}
+),
