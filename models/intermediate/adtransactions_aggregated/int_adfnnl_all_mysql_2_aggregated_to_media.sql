c964b3373579748472ac2ec3edd7acbec6b5186f
baiyunfan-jk <140997291+baiyunfan-jk@users.noreply.github.com>
2023-08-01 16:40:36 +0800

    Update int_adfnnl_all_mysql_2_aggregated_to_media.sql

--- models/intermediate/adtransactions_aggregated/int_adfnnl_all_mysql_2_aggregated_to_media.sql
+++ models/intermediate/adtransactions_aggregated/int_adfnnl_all_mysql_2_aggregated_to_media.sql
@@ -1,1 +1,18 @@
 
+{{ config(
+  materialized = 'incremental',
+  incremental_strategy = 'insert_overwrite',
+)}}
+with source as {{ref('ad_fnnl_all')}},
+incremental_table_data as (
+  select a.*,b.min_dt,b.max_dt
+  from source a
+  left join (
+  select media,config_id_name,strategys_name,
+  min(dt) min_dt,
+  max(dt) max_dt,
+  from source
+  group by 1,2,3
+  )b on a.media=b.media and a.config_id_name=b.config_id_name and a.strategys_name=b.strategys_name
+)
+select * from incremental_table_data
