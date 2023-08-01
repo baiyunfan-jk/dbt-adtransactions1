37f9271f972a15c14446d4f83e033f9d60acbc97
baiyunfan-jk <140997291+baiyunfan-jk@users.noreply.github.com>
2023-08-01 15:03:19 +0800

    Update _adtransactions_aggregated_to_media_bckt_accgroup_front.sql

--- models/intermediate/adtransactions_aggregated/_adtransactions_aggregated_to_media_bckt_accgroup_front.sql
+++ models/intermediate/adtransactions_aggregated/_adtransactions_aggregated_to_media_bckt_accgroup_front.sql
@@ -1,8 +1,8 @@
-{{ config(materialized='view') }}
+{{ config(materialized='ephemeral') }}
 
 with source as (
 
-    select * from {{ source('ad_transactions', 'dm_eco_dataeco_ad_transations_0_4_1') }} 
+    select * from {{ ref('ad_transaction_detail') }} 
 
 ),
 with renamed as (
@@ -13,19 +13,21 @@ with renamed as (
         count(distinct clk_device_md5) as clk_uv,
         sum(imp_cpm_cost) as imp_cpm_cost
     from (
-    select *
-         , case when length(imp_device_id)=32 then imp_device_id
-           when length(imp_oaid_md5)=32 then imp_oaid_md5
-           when length(imp_android_id)=32 then imp_android_id
-          else null end as imp_device_md5--集中设备号到一个字段,计算UV
-         , case when length(clk_device_id)=32 then clk_device_id
-           when length(clk_oaid_md5)=32 then clk_oaid_md5
-           when length(clk_android_id)=32 then clk_android_id
-          else null end as clk_device_md5--集中设备号到一个字段,计算UV
-        , sum(imp_agg_ct) as imp_pv
-        , sum(clk_agg_ct) as clk_pv
-        , sum(imp_cpm_cost) as imp_cpm_cost
+    select p_day, p_resource_code, win_config_id, req_bucket
+    , imp_device_md5--集中设备号到一个字段,计算UV
+    , clk_device_md5--集中设备号到一个字段,计算UV
+    , sum(imp_agg_ct) as imp_pv
+    , sum(clk_agg_ct) as clk_pv
+    , sum(imp_cpm_cost) as imp_cpm_cost
+    from (
+    select p_day, p_resource_code, win_config_id, req_bucket
+    , imp_device_md5
+    , clk_device_md5
+    , sum(imp_agg_ct) as imp_pv
+    , sum(clk_agg_ct) as clk_pv
+    , sum(imp_cpm_cost) as imp_cpm_cost
     from source
+    )
     where p_day >=date_format(date_sub(date('${pDate}') ,7),'yyyyMMdd')
     group by 1,2,3,4,5,6
     )
